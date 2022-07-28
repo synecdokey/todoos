1813e7086a843c399637e69f6962c44012a6e987
Emilia Zapata <zapata.contact@gmail.com>
2022-07-28 16:06:29 +1200

    feat(todoos): add auth and todos app

--- src/server/router/protected-example-router.ts
+++ src/server/router/protected-example-router.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createProtectedRouter } from "./protected-router";
 
 // Example router with queries that can only be hit if the user requesting is signed in
@@ -10,5 +11,26 @@ export const protectedExampleRouter = createProtectedRouter()
   .query("getSecretMessage", {
     resolve({ ctx }) {
       return "He who asks a question is a fool for five minutes; he who does not ask a question remains a fool forever.";
+    },
+  })
+  .mutation("addTodo", {
+    input: z.object({ text: z.string() }),
+    resolve({ ctx, input }) {
+      return ctx.prisma.todo.create({
+        data: { userId: ctx.session.user.id!, text: input.text },
+      });
+    },
+  })
+  .mutation("removeTodo", {
+    input: z.object({ id: z.string() }),
+    resolve({ ctx, input }) {
+      return ctx.prisma.todo.delete({ where: input });
+    },
+  })
+  .query("getTodos", {
+    resolve({ ctx }) {
+      return ctx.prisma.todo.findMany({
+        where: { userId: ctx.session.user.id },
+      });
     },
   });
